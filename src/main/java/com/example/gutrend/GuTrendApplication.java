134a5605a6d3783c5571ef769be2bccba755c870
SonJava10Rikkei <sonjapan7@gmail.com>
2023-06-20 01:17:05 +0700

    23-06-20 category done

--- src/main/java/com/example/gutrend/GuTrendApplication.java
+++ src/main/java/com/example/gutrend/GuTrendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo2Application {
+public class GuTrendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo2Application.class, args);
+        SpringApplication.run(GuTrendApplication.class, args);
     }
 
 }
